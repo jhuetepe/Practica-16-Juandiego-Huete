7cf3345449fe456fb78519e5eee192b55973befd
jhuetepe <5292xrh9hc@privaterelay.appleid.com>
2026-05-05 13:11:47 +0200

    actualización

--- Práctica 16.R
+++ Práctica 16.R
@@ -2,3 +2,15 @@
 ##Juandiego Huete Petisco
 # Tue May  5 12:46:49 2026 ------------------------------
 
+library(haven)
+library(tidyverse)
+library(ggplot2)
+library(knitr)
+
+pisa <- read_sav("PISA22SP.sav")
+pisa <- na.omit(pisa)  # Eliminar filas con NA
+plot(pisa$lectu, pisa$mates)
+
+reg <- lm(mates ~ lectu, data = pisa)
+summary(reg)
+
